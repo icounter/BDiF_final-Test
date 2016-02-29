87b969665725cebc0bc3daf9476b7c61300386da
peterchaozhu <peterchaoz93@gmail.com>
2016-02-29 15:27:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,14 +14,16 @@
 using namespace std;
 using namespace boost::posix_time;
 using namespace boost::gregorian;
-
+//this is to define default time
 ptime time_t_epoch(date(1970, 1, 1));
+//define a struct to store data
 struct record{
     date  date1;
     ptime time;
     float price;
     int volume;
 };
+//a function to read line by','
 vector<string> splitline(char* buff){
     string a=string(buff);
     vector<string> result;
@@ -33,24 +35,22 @@ vector<string> splitline(char* buff){
         }
     }
     if(a[a.size()-1]!=',') result.push_back(a.substr(j,a.size()-j));
-//    for(int i=0;i<result.size();i++){
-//        cout<<result[i]<<endl;
-//    }
     return result;
 }
+//this is to calculate mean and stand devation of a vector
 pair<float,float> calculate_mean_sd(vector<float> resultSet){
     double sum = std::accumulate(std::begin(resultSet), std::end(resultSet), 0.0);
-    float mean =  sum / resultSet.size(); //均值
+    float mean =  sum / resultSet.size(); //mean
 
     float accum  = 0.0;
     std::for_each (std::begin(resultSet), std::end(resultSet), [&](const float d) {
         accum  += (d-mean)*(d-mean);
     });
 
-    float stdev = sqrt(accum/(resultSet.size()-1)); //方差
+    float stdev = sqrt(accum/(resultSet.size()-1)); //stdev
     return make_pair(mean,stdev);
 }
-
+//this function is to return the stats of 3 sigma,data out of 3 sigma is noise(for quote I think as long as it is not negative is okay) 
 vector<float> stat(vector<record> buff){
     vector<float> price;
     vector<float> volume;
@@ -75,6 +75,7 @@ vector<float> stat(vector<record> buff){
     result.push_back(vol_mean+3*vol_sd);
     return result;
 }
+//convert string to struct
 struct record convert_string_to_record(vector<string> line){
     struct record record1;
     date d(from_undelimited_string(line[0].substr(0,8)));
@@ -89,11 +90,11 @@ struct record convert_string_to_record(vector<string> line){
     record1.volume=volume;
     return record1;
 }
+//define compae funtion
 bool Compare(pair<string,int> a1,pair<string,int> a2){
     return a1.second<a2.second;
 }
-
-
+//process data
 void process(char *buff,vector<record> &valid,vector<record> &noise){
     char *pch;
     pch=strtok(buff,"\n");
@@ -103,19 +104,22 @@ void process(char *buff,vector<record> &valid,vector<record> &noise){
         vector<string> line=splitline(pch);
         if(line.size()==3&&line[0].size()==24){//this is potential right answer
             struct record temp=convert_string_to_record(line);
+            //price and volume cant be negative or too large
             if(temp.price<=0||temp.volume<=0||temp.price>100000||temp.volume>10000000){
                 noise.push_back(temp);
             }
             else {
                 temp_vector.push_back(temp);
                 i++;
+                //read every 10000 lines, and calculate the mean and standev
                 if (i > 10000) {
                     i = i - 10000;
                     vector<float> min_max_number = stat(temp_vector);
-                   // cout<<min_max_number[0]<<endl;
                     priority_queue<pair<string, int>, vector<pair<string, int>>, function<bool(pair<string, int>,
                                                                                            pair<string, int>)>> count_date(
                             Compare);
+                    //the below get the most common date of the vector and treat all the other dates(second common if they are more than 100 is also possible)
+                    //are noise
                     unordered_map<string,int> map;
                     for(int i=0;i<temp_vector.size();i++){
                         unordered_map<string,int>::iterator got = map.find (to_simple_string(temp_vector[i].date1));
@@ -152,6 +156,7 @@ void process(char *buff,vector<record> &valid,vector<record> &noise){
         }
         pch = strtok (NULL, "\n");
     }
+    //the last 10000 lines do the same things as ablove
     if(i!=0&&temp_vector.size()!=0){
         vector<float> min_max_number=stat(temp_vector);
         cout<<min_max_number[0]<<"  "<<min_max_number[1]<<min_max_number[2]<<min_max_number[3]<<endl;
@@ -188,13 +193,14 @@ void process(char *buff,vector<record> &valid,vector<record> &noise){
         temp_vector.clear();
     }
 }
+//convert ptime to string 
 string convert_ptime_to_string(ptime time){
     string a=to_iso_extended_string(time);
     string b;
     b=a.substr(0,4)+a.substr(5,2)+a.substr(8,2)+':'+a.substr(11,8)+'.'+a.substr(20,6);
     return b;
-
 }
+//get current time to out put the time usage
 int64_t GetCurrentStamp64()
 {
     boost::posix_time::ptime epoch(boost::gregorian::date(1970, boost::gregorian::Jan, 1));
@@ -203,8 +209,8 @@ int64_t GetCurrentStamp64()
     return time_from_epoch.total_microseconds();
 
 }
-/////////////normality
-
+/////////////normality test
+/////and the normal class is in the main.hpp
 void normal_test(vector<record> valid,float &param,long long ndim){
 vector<float> vec;
     for(int i=0;i<valid.size();i++){
@@ -215,48 +221,41 @@ vector<float> vec;
     ndim=vec.size();
 
 }
-
-
-
-
-
-
-
-
-
 int main(int argc, char** argv) {
     int rank, size;
     long long read_cnt;
     long long filesize;
     MPI_File fh,fhv,fhn,fhl;
     MPI_Status status;
+    //initialize
     MPI_Init(&argc, &argv);
+    //get rank
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    //get size
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     char* FILENAME=argv[1];
     int64_t t1=GetCurrentStamp64();
+    //open file
     int ret= MPI_File_open(MPI_COMM_WORLD,FILENAME,MPI_MODE_RDONLY,MPI_INFO_NULL,&fh);
+    //get filze size
     MPI_File_get_size(fh,&filesize);
+    //get local_size every process need to read how large data
     long long local_size=filesize/size;
     read_cnt=(rank==size-1)?((filesize-(size-1)*local_size)):local_size;
     char *buff=(char *)(malloc(sizeof(char)*(read_cnt+1)));
     if(buff==NULL) {cout<<"error!"; return 0;}
-  MPI_File_read_at(fh, rank*local_size, buff, read_cnt, MPI_BYTE, &status);
+    //read begin stroe data to buffer
+    MPI_File_read_at(fh, rank*local_size, buff, read_cnt, MPI_BYTE, &status);
+    //get read time
     int64_t t2=GetCurrentStamp64();
-
+    //set valid and noise vector
     vector<record> valid;
     vector<record> noise;
-   process(buff,valid,noise);
+    //process data
+    process(buff,valid,noise);
     int64_t t3=GetCurrentStamp64();
-   char *valid_out=new char[2*read_cnt];
-//   for(int i=0;i<valid.size();i++){
-//       cout<<to_simple_string(valid[i].time)<<","<<to_string(valid[i].price)<<","<<to_string(valid[i].volume)<<endl;
-////    }
-//    for(int i=0;i<noise.size();i++){
-//        cout<<to_simple_string(noise[i].time)<<","<<to_string(noise[i].price)<<","<<to_string(noise[i].volume)<<endl;
-//    }
-   //cout<<"noise.size"<<noise.size()<<endl;
-char* p;
+    char *valid_out=new char[2*read_cnt];
+    char* p;
     p=valid_out;
     for(int i=0;i<valid.size();i++){
         string a=convert_ptime_to_string(valid[i].time)+','+to_string(valid[i].price)+','+to_string(valid[i].volume);
@@ -275,8 +274,8 @@ char* p;
         q+=b.length();
         *q='\n';
         q++;
-   }
-//    strcpy(noise_out,b.c_str());
+    }
+    //    strcpy(noise_out,b.c_str());
     MPI_File_open(MPI_COMM_WORLD,"valid_data.txt",MPI_MODE_WRONLY|MPI_MODE_CREATE,MPI_INFO_NULL,&fhv);
     MPI_File_set_size(fhv,0);
     MPI_File_open(MPI_COMM_WORLD,"noise_data.txt",MPI_MODE_WRONLY|MPI_MODE_CREATE,MPI_INFO_NULL,&fhn);
@@ -299,18 +298,10 @@ char* p;
     //cout<<valid_allsz[rank]<<endl;
     MPI_File_write_at_all(fhv,valid_allsz[rank]-write_valid_size,valid_out,write_valid_size,MPI_BYTE,&status);
     MPI_File_write_at_all(fhn,noise_allsz[rank]-write_noise_size,noise_out,write_noise_size,MPI_BYTE,&status);
-    //splitfile(buff);//plitfile buff
-//for(int i=0;i<read_cnt;++i){
-//    cout<<buff[i];
-//}
-
-//cout<<"||||||||||"<<endl;
-//    long write_size=read_cnt/2;
-//    int allsz[size];
-
+    //free content
     free(noise_out);
-
     free(buff);
+    //close file
     MPI_File_close(&fhv);
     MPI_File_close(&fhn);
     MPI_File_close(&fh);
@@ -335,6 +326,7 @@ char* p;
     MPI_File_close(&fhl);
     float JB_factor;
     long long ndim;
+    //out put mormality test
     normal_test(valid,JB_factor,ndim);
     stringstream norm;
     norm<<"rank: "<<rank<<"JB stats is"<<JB_factor<<"in how many data points"<<ndim<<endl;
